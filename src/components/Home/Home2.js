e75638cebc3cb527fc9552e57711f673ea2e76f9
Vaikunthkeshav <vakr3628@colorado.edu>
2025-06-22 16:11:41 -0600

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,36 +1,40 @@
-// Find this section and update it:
-<p className="home-about-body">
-  I am currently pursuing my <b className="purple">M.S. in Mechanical Engineering</b> at 
-  <i>
-    <b className="purple"> University of Colorado Boulder. </b>
-  </i>
-  <br />
-  <br />I am fluent in classics like
-  <i>
-    <b className="purple"> SolidWorks, AutoCAD, and Python. </b>
-  </i>
-  <br />
-  <br />
-  My field of Interest's are building new &nbsp;
-  <i>
-    <b className="purple">Robotics and Mechanical Design Projects </b> and
-    also in areas related to{" "}
-    <b className="purple">
-      Design for Manufacturability.
-    </b>
-  </i>
-  <br />
-  <br />
-  Whenever possible, I also apply my passion for developing products
-  with <b className="purple">CAD Software</b> and
-  <i>
-    <b className="purple">
-      {" "}
-      Modern Engineering Tools
-    </b>
-  </i>
-  &nbsp; like
-  <i>
-    <b className="purple"> Ansys and Abaqus</b>
-  </i>
-</p>
+// src/Home2.js or src/components/Home2.js
+
+const Home2 = () => {
+  return (
+    <div className="home-about-body">
+      I am currently pursuing my <b className="purple">M.S. in Mechanical Engineering</b> at 
+      <i>
+        <b className="purple"> University of Colorado Boulder. </b>
+      </i>
+      <br />
+      <br />I am fluent in classics like
+      <i>
+        <b className="purple"> SolidWorks, AutoCAD, and Python. </b>
+      </i>
+      <br />
+      <br />
+      My field of Interest's are building new &nbsp;
+      <i>
+        <b className="purple">Robotics and Mechanical Design Projects </b> and
+        also in areas related to{" "}
+        <b className="purple">
+          Design for Manufacturability.
+        </b>
+      </i>
+      <br />
+      <br />
+      Whenever possible, I also apply my passion for developing products
+      with <b className="purple">CAD Software</b> and
+      <i>
+        <b className="purple"> Modern Engineering Tools </b>
+      </i>
+      &nbsp; like
+      <i>
+        <b className="purple"> Ansys and Abaqus</b>
+      </i>
+    </div>
+  );
+};
+
+export default Home2;
